8632174128a25c8ef74a37407d757165cb8ede51
Bas van Wouwen <bas.vanwouwen@bitfactory.nl>
2021-01-31 19:59:12 +0100

    Reset package version in copy script

--- .bin/copy-component.sh
+++ .bin/copy-component.sh
@@ -15,16 +15,7 @@ kebabComponent=$(echo "$component" | perl -pe 's/([a-z0-9])([A-Z])/$1-\L$2/g' | 
 
 echo "Copying $originalComponent to new compontent directory: $component";
 
-scp -rp ./src/components/$originalComponent/. ./src/components/$component/
-
-echo "Remove /dist folder";
-rm -rf ./src/components/$component/dist
-
-echo "Remove /node_modules folder";
-rm -rf ./src/components/$component/node_modules
-
-echo "Remove /package-lock.json";
-rm -rf ./src/components/$component/package-lock.json
+rsync -a --exclude={'dist','node_modules','package-lock.json'} ./src/components/$originalComponent/ ./src/components/$component/
 
 echo "Rename component to $component.vue";
 mv ./src/components/$component/src/$originalComponent.vue ./src/components/$component/src/$component.vue
@@ -38,5 +29,8 @@ echo "Replace $kebabOriginalComponent => $kebabComponent occurences in files";
 perl -pi -e "s/$kebabOriginalComponent/$kebabComponent/g" ./src/components/$component/package.json
 perl -pi -e "s/$kebabOriginalComponent/$kebabComponent/g" ./src/components/$component/README.md
 perl -pi -e "s/$kebabOriginalComponent/$kebabComponent/g" ./src/components/$component/src/$component.vue
+
+echo "Reset package version";
+perl -pi -e "s/\"version\": \"([0-9]+).([0-9]+).([0-9]+)\"/\"version\": \"0.0.0\"/g" ./src/components/$component/package.json
 
 echo "Done!";
